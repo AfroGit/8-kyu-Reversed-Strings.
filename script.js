02246790d9adfa5e43e17a4fe0f8bec5ccf1e961
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2022-11-09 23:07:00 +0000

    Added the Reverse( ) Method.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 "use strict";
 
-function solution(str){
-  return str.split("").reverse().join("");
-  
-}
+function reverseNoun(str){
+  // return str.split("").reverse().join("");
+  splitNoun = str.split("");
+  reverseArray = splitNoun.reverse();
+  joinArray = reverseArray.join("");
+  return joinArray;
+  }
+   console.log(reverseNoun("This Was Fun"))
+
